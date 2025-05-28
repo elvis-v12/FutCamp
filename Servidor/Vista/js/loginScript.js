152bfa895b733cac9a7e490dfea956fc95118326
dalfap <dikealfredo25@gmail.com>
2025-05-28 09:19:03 -0500

    {Correción en index.html e implementación de r-password}

--- Servidor/Vista/js/loginScript.js
+++ Servidor/Vista/js/loginScript.js
@@ -1,69 +1,13 @@
-// Variable que almacena el índice del dígito actual ingresado
-let currentDigitIndex = 0;
+const container = document.getElementById('container');
+const overlayCon = document.getElementById('overlayCon');
+const overlayBtn = document.getElementById('overlayBtn');
 
-// Constante que define el número máximo de dígitos permitidos en la contraseña
-const maxDigits = 8;
+overlayBtn.addEventListener('click', () => {
+    container.classList.toggle('right-panel-active');
 
-// Función para agregar un dígito al campo de contraseña
-function addDigit(digit) {
-    // Verifica si el número actual de dígitos ingresados es menor que el máximo permitido
-    if (currentDigitIndex < maxDigits) {
-        // Obtiene el elemento de verificación correspondiente al dígito actual
-        const checkElement = document.getElementById(`mrk${currentDigitIndex + 1}`);
-        // Obtiene el campo de contraseña
-        const passwordField = document.getElementById('contrasena');
+    overlayBtn.classList.remove('btnScaled');
+    window.requestAnimationFrame( () => {
+        overlayBtn.classList.add('btnScaled');
+    });
+});
 
-        // Verifica si los elementos existen
-        if (checkElement && passwordField) {
-            // Agrega la clase 'filled' al elemento de verificación
-            checkElement.classList.add('filled');
-            // Agrega el dígito al campo de contraseña
-            passwordField.value += digit;
-            // Incrementa el índice del dígito actual
-            currentDigitIndex++;
-        }
-    }
-}
-
-// Función para eliminar el último dígito del campo de contraseña
-function removeDigit() {
-    // Verifica si hay dígitos ingresados para eliminar
-    if (currentDigitIndex > 0) {
-        // Decrementa el índice del dígito actual
-        currentDigitIndex--;
-        // Obtiene el elemento de verificación correspondiente al dígito actual
-        const checkElement = document.getElementById(`mrk${currentDigitIndex + 1}`);
-        // Obtiene el campo de contraseña
-        const passwordField = document.getElementById('contrasena');
-
-        // Verifica si los elementos existen
-        if (checkElement && passwordField) {
-            // Remueve la clase 'filled' del elemento de verificación
-            checkElement.classList.remove('filled');
-            // Elimina el último dígito del campo de contraseña
-            passwordField.value = passwordField.value.slice(0, -1);
-        }
-    }
-}
-
-// Función para limpiar todos los dígitos ingresados en el campo de contraseña
-function clearDigits() {
-    // Itera sobre todos los elementos de verificación
-    for (let i = 1; i <= maxDigits; i++) {
-        const checkElement = document.getElementById(`mrk${i}`);
-        // Verifica si el elemento existe
-        if (checkElement) {
-            // Remueve la clase 'filled' del elemento de verificación
-            checkElement.classList.remove('filled');
-        }
-    }
-    // Obtiene el campo de contraseña
-    const passwordField = document.getElementById('contrasena');
-    // Verifica si el campo de contraseña existe
-    if (passwordField) {
-        // Limpia el campo de contraseña
-        passwordField.value = '';
-    }
-    // Reinicia el índice del dígito actual
-    currentDigitIndex = 0;
-}
