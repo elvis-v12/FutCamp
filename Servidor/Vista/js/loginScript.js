8abc65c1ba045589b19fa391f9294f9045b6df42
dalfap <dikealfredo25@gmail.com>
2025-05-31 19:40:53 -0500

    {Adición de responsive desing}

--- Servidor/Vista/js/loginScript.js
+++ Servidor/Vista/js/loginScript.js
@@ -1,13 +1,14 @@
 const container = document.getElementById('container');
-const overlayCon = document.getElementById('overlayCon');
-const overlayBtn = document.getElementById('overlayBtn');
+const allOverlayButtons = document.querySelectorAll('.overlay-panel button');
 
-overlayBtn.addEventListener('click', () => {
-    container.classList.toggle('right-panel-active');
-
-    overlayBtn.classList.remove('btnScaled');
-    window.requestAnimationFrame( () => {
-        overlayBtn.classList.add('btnScaled');
+allOverlayButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        container.classList.toggle('right-panel-active');
     });
 });
-
+const overlayBtn = document.getElementById('overlayBtn');
+if (overlayBtn) {
+    overlayBtn.addEventListener('click', () => {
+        container.classList.toggle('right-panel-active');
+    });
+}
